912989116d523319eb7c42a12e108a3246becf5c
ee7klt <kltah1@gmail.com>
2015-11-26 10:01:34 +0800

    add toggleClass

--- bind.js
+++ bind.js
@@ -1,5 +1,8 @@
 $('#opendiv').on('click', function() {
   console.log(this)
-  $(this).toggleClass("active");
-  $('#div1').slideToggle();
+    $(this).toggleClass("active")
+    $('#div1').slideToggle(200, function() {
+      console.log(this)
+      //$(this).toggleClass("Active")
+    });
 })
